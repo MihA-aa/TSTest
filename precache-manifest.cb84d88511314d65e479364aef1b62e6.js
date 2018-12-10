3804ef6acec5eea6e2ee0660eb2ca89c5a8baba6
m.ermolaev <m.ermolaev@itransition.com>
2018-12-11 01:44:48 +0300

    Updates

---
+++ precache-manifest.cb84d88511314d65e479364aef1b62e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5687ffafa0ff04fd2132",
+    "url": "/task7/static/css/main.9ba02f99.chunk.css"
+  },
+  {
+    "revision": "5687ffafa0ff04fd2132",
+    "url": "/task7/static/js/main.5687ffaf.chunk.js"
+  },
+  {
+    "revision": "9bca741a2f52c1ea8a28",
+    "url": "/task7/static/js/1.9bca741a.chunk.js"
+  },
+  {
+    "revision": "15c201686d13b874692f",
+    "url": "/task7/static/js/runtime~main.15c20168.js"
+  },
+  {
+    "revision": "6859eaed9634e6944d13a621a49efa00",
+    "url": "/task7/index.html"
+  }
+];
